d9c04f258399c01f138486b3f9782add36c831df
iqbaladinur <iqbaladinur@gmail.com>
2025-08-05 16:02:25 +0700

    fix: include year in date formatting for TransactionItem component

--- src/components/TransactionItem.vue
+++ src/components/TransactionItem.vue
@@ -101,6 +101,7 @@ const formatDate = (dateString: string) => {
   return new Date(dateString).toLocaleDateString('en-US', {
     month: 'short',
     day: 'numeric',
+    year: 'numeric'
   });
 };
 </script>
